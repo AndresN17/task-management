856002ac1cbf69cb4862c71ba40b7958f8772a5a
Andres Noboa <andresnoboa17@hotmail.com>
2021-05-19 00:26:07 -0500

    Added user singUp with hashedPassword

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,15 +1,31 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    try {
+      const { username, password } = authCredentialsDto;
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
 
-    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
-        const {username, password} = authCredentialsDto;
-        const user = new User();
-        user.username = username;
-        user.password = password;
-        await user.save();
+      const user = new User();
+      user.username = username;
+      user.password = hashedPassword;
+      user.salt = salt;
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists!');
+      } else {
+        throw new InternalServerErrorException();
+      }
     }
+  }
 }
