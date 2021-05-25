4168b5cfc998847b845ba1c1174ad0fe5e437707
Andres Noboa <andresnoboa17@hotmail.com>
2021-05-24 23:57:54 -0500

    Doing more bulletproof the testing

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -13,18 +13,20 @@ class FriendList {
 
 //test
 describe('Friendlist', () => {
+  let friendList;
+  beforeEach(() => {
+    friendList = new FriendList();
+  });
   it('initialize friendlist', () => {
-    const friendList = new FriendList();
     expect(friendList.friends.length).toEqual(0);
   });
   it('adds a friend to the list', () => {
-    const friendList = new FriendList();
     friendList.addFriend('Nobara Kugisaki');
     expect(friendList.friends.length).toEqual(1);
   });
   it('announces friendship', () => {
-    const friendList = new FriendList();
     friendList.announceFriendship = jest.fn();
+    expect(friendList.announceFriendship).not.toHaveBeenCalled();
     friendList.addFriend('Nobara Kugisaki');
     expect(friendList.announceFriendship).toHaveBeenCalled();
   });
