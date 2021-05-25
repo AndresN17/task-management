50222fee8b7862068f029727deeefb8e8cfa310f
Andres Noboa <andresnoboa17@hotmail.com>
2021-05-24 23:46:29 -0500

    Created a fake class and testing it

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,5 +1,20 @@
-describe('my test', () => {
-    it('returns true',()=>{
-        expect(true).toEqual(true);
-    })
-});
+class FriendList {
+  friends = [];
+
+  addFriend(name: string) {
+    this.friends.push(name);
+  }
+}
+
+//test
+describe("Friendlist",()=>{
+    it('initialize friendlist',()=>{
+        const friendList = new FriendList();
+        expect(friendList.friends.length).toEqual(0);
+    });
+    it('adds a friend to the list',()=>{
+        const friendList= new FriendList();
+        friendList.friends.push("Nobara Kugisaki");
+        expect(friendList.friends.length).toEqual(1);
+    });
+})
