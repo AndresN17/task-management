23ca412ac8c5123b1f32dcb71869847822194898
Andres Noboa <andresnoboa17@hotmail.com>
2021-05-24 23:52:48 -0500

    Added a mock to see how many times a function was called

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -3,18 +3,29 @@ class FriendList {
 
   addFriend(name: string) {
     this.friends.push(name);
+    this.announceFriendship(name);
+  }
+
+  announceFriendship(name: string) {
+    console.log(name);
   }
 }
 
 //test
-describe("Friendlist",()=>{
-    it('initialize friendlist',()=>{
-        const friendList = new FriendList();
-        expect(friendList.friends.length).toEqual(0);
-    });
-    it('adds a friend to the list',()=>{
-        const friendList= new FriendList();
-        friendList.friends.push("Nobara Kugisaki");
-        expect(friendList.friends.length).toEqual(1);
-    });
-})
+describe('Friendlist', () => {
+  it('initialize friendlist', () => {
+    const friendList = new FriendList();
+    expect(friendList.friends.length).toEqual(0);
+  });
+  it('adds a friend to the list', () => {
+    const friendList = new FriendList();
+    friendList.addFriend('Nobara Kugisaki');
+    expect(friendList.friends.length).toEqual(1);
+  });
+  it('announces friendship', () => {
+    const friendList = new FriendList();
+    friendList.announceFriendship = jest.fn();
+    friendList.addFriend('Nobara Kugisaki');
+    expect(friendList.announceFriendship).toHaveBeenCalled();
+  });
+});
